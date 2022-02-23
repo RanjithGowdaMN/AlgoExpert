12f244c8f2d40d0cf0737e3ff0626464236cddc6
Ranjith Gowda M N <ranjith.1409@gmail.com>
2022-02-23 12:49:07 +0300

    Folder/File Updates

---
+++ GreadyAlgorithm/00 Easy/MiniumWaitingTime.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreadyAlgorithm._00_Easy
+{
+    internal class MiniumWaitingTime
+    {
+    }
+}
