807af1461d99f8deb3f66b84eead038dc2c95d29
Ranjith <rnagaraja@moi.gov.qa>
2023-08-25 16:22:06 +0300

    test update

---
+++ AlgoExpertTestCases/ArraysTest/TwoNumberSum.cs
@@ -0,0 +1,21 @@
+﻿using Arrays._01_Easy;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoExpertTestCases
+{
+    [TestClass]
+    class TwoNumberSum
+    {
+        [TestMethod]
+        public void TestCaseX()
+        {
+            Assert.AreEqual(CTwoNumberSum.TwoNumberSum(new int[] { 3, 5, -4, 8, 11, 1, -1, 6 }, 10), new int[] { 11, -1 });
+        }
+    }
+}
